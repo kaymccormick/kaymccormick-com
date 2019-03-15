22badeb6c6094e747b5d798ccf6352dd2883d18c
Kay McCormick <kay@kaymccormick.com>
2019-03-14 19:17:41 -0700

    config

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    presets: ['@babel/preset-env', '@babel/preset-react', '@emotion/babel-preset-css-prop', 'next/babel'],
+    plugins: ['emotion', '@babel/plugin-syntax-dynamic-import']
+};
