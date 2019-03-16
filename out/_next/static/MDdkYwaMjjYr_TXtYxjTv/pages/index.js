fcf86765a1da02860311543967c6e270c4ef63c9
Kay McCormick <kay@kaymccormick.com>
2019-03-16 05:21:10 -0700

    remove out

--- out/_next/static/MDdkYwaMjjYr_TXtYxjTv/pages/index.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RNiq:function(n,t,e){"use strict";e.r(t);e("qKvR");var i=e("q1tI"),o=e.n(i),a=(e("b0oO"),e("5Yp1"));t.default=function(n){return o.a.createElement(a.a,{title:"Kay McCormick"})}},vlRD:function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){var n=e("RNiq");return{page:n.default||n}}])}},[["vlRD",1,0]]]);
